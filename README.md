e7a4dffe001aa102c886515f96afa3ee234ceeb5
Patrickill <1322301665@qq.com>
2024-11-26 14:27:59 +0800

    init: 初始化开发环境

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# forum
-javaee课设
+# 论坛系统
+
+作为JavaEE+软件工程的课设
+
+技术栈选取了react+zustand+chakraUI
---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // 如果编辑器提示 path 模块找不到，则可以安装一下 @types/node -> pnpm i @types/node -D
+
+function resolve(dir: string) {
+  return path.join(__dirname, '.', dir);
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // 配置别名
+  resolve: {
+    alias: {
+      '@': resolve('src'), // 设置 `@` 指向 `src` 目录
+    },
+  },
+});
